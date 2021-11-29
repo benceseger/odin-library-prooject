ed559ff6bcfbc54723288b507db778cd8f27dfdb
Bence Seger <benceseger3@gmail.com>
2021-11-29 09:27:23 +0100

    Project Added to Repository

---
+++ index.js
@@ -0,0 +1,22 @@
+const addBookButton = document.getElementById('addBook');
+
+let myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary() {
+    // executes the command after user fills everything in the input form and clicks the button
+}
+
+function displayInputWindow() {
+    // displays the input window so user can input a book
+}
+
+addBookButton.addEventListener('click', () => {
+
+})
